515f4a310be5f951bd2cbefb893fc04acc2b9595
Observer-L <observer_l@163.com>
2018-03-08 21:04:48 +0800

    Web Design Effects Series - 2

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 
 ## 	Web Design Effects Series
 [Split Screen Slider](http://www.elpsycongroo.cc/WebProjects/Split%20Screen%20Slider/index.html)
+
+[Split Landing Page](http://www.elpsycongroo.cc/WebProjects/Split%20Landing%20Page/index.html)
---
+++ Split Landing Page/js/main.js
@@ -0,0 +1,18 @@
+var left = document.querySelector('.split.left');
+var right = document.querySelector('.split.right');
+const container = document.querySelector(".container");
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+})
+left.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+    container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-right");
+});
