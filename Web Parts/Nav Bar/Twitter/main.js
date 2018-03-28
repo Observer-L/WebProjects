c079e19c40f1d0138ee0f6ae32bb5c757f9a3103
Observer-L <observer_l@163.com>
2018-03-28 22:41:04 +0800

    add a dropdown menu to navbar

--- Web Parts/Nav Bar/Twitter/main.js
+++ Web Parts/Nav Bar/Twitter/main.js
@@ -1,7 +1,18 @@
 (function activeTabs() {
   let tabs = document.querySelectorAll('.tabs ul li');
-  [].forEach.call(tabs, tab=>tab.addEventListener('click', (e)=>{
-    [].forEach.call(tabs, tab=>{tab.classList.remove('active')});
+  [].forEach.call(tabs, tab => tab.addEventListener('click', (e) => {
+    [].forEach.call(tabs, tab => {
+      tab.classList.remove('active')
+    });
     tab.classList.add('active');
   }))
+}());
+
+(function showDropDownMenu() {
+  let accout = document.querySelector('.accout');
+  let dropDownMenu = accout.querySelector('.dropdown-menu');
+
+  accout.addEventListener('click', (e)=>{
+    dropDownMenu.classList.toggle('show');
+  })
 }())
