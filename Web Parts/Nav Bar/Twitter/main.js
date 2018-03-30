b0f46c1e6cf5cbe30ceaa7a4cb369977e699c53e
Observer-L <observer_l@163.com>
2018-03-30 15:19:01 +0800

    1.add keyword tips(test) to search bar. 2.click the Tweet button popup the editor window

--- Web Parts/Nav Bar/Twitter/main.js
+++ Web Parts/Nav Bar/Twitter/main.js
@@ -39,11 +39,9 @@
 
 
 
-
 function hoverText() {
   let accout = document.querySelector('.accout');
   if (!accout.querySelector('.hover-text')) {
-    console.log(1);
     let div = document.createElement('div');
     div.className = 'hover-text';
     let text = document.createElement('span');
@@ -61,3 +59,48 @@ function hoverText() {
 
 let accout = document.querySelector('.accout');
 accout.addEventListener('mouseover', hoverText, false);
+
+
+
+(function tweet() {
+  let tweet_btn = document.getElementsByClassName('tweet')[0];
+  let content = document.getElementsByClassName('content')[0];
+  let filter = document.getElementById('filter');
+
+
+  tweet_btn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    if (!document.getElementsByClassName('tweet-frame')[0]) {
+      let tweetFrame = document.createElement('div');
+      tweetFrame.className = 'tweet-frame';
+      tweetFrame.innerHTML = `
+      <div class="modal-header"><h3 class="modal-title">Compose new Tweet</h3></div>
+      <div class="modal-body">
+      <div id="tweet-box">
+      <img src="img/default_profile_normal.png" class="avatar" />
+      <div id="editor" contentEditable="true" data-placeholder="What’s happening?"></div>
+      </div>
+      <button class="btn close"><i class="fas fa-times"></i></button>
+      </div>`;
+      content.appendChild(tweetFrame);
+      filter.style.display = 'block';
+
+
+      let closeBtn = document.getElementsByClassName('close')[0];
+      closeBtn.addEventListener('click', (e) => {
+        console.log('btn');
+        content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
+        filter.style.display = 'none';
+      })
+    }
+  })
+
+  // 与前一种点击页面其他地方隐藏div的实现方法不同，这里利用div后面的遮罩层，给遮罩层绑定事件实现
+  filter.addEventListener('click', (e) => {
+    if (document.getElementsByClassName('tweet-frame')[0]) {
+      content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
+      filter.style.display = 'none';
+    }
+  })
+
+}())
