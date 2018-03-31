64ef03011f473d1920f19675ca535c16488b0819
Observer-L <observer_l@163.com>
2018-03-31 12:03:12 +0800

    edit content discard confirm

--- Web Parts/Nav Bar/Twitter/main.js
+++ Web Parts/Nav Bar/Twitter/main.js
@@ -61,12 +61,10 @@ let accout = document.querySelector('.accout');
 accout.addEventListener('mouseover', hoverText, false);
 
 
-
 (function tweet() {
   let tweet_btn = document.getElementsByClassName('tweet')[0];
   let content = document.getElementsByClassName('content')[0];
-  let filter = document.getElementById('filter');
-
+  let filter = document.getElementsByClassName('filter')[0];
 
   tweet_btn.addEventListener('click', (e) => {
     e.stopPropagation();
@@ -88,19 +86,74 @@ accout.addEventListener('mouseover', hoverText, false);
 
       let closeBtn = document.getElementsByClassName('close')[0];
       closeBtn.addEventListener('click', (e) => {
-        console.log('btn');
-        content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
-        filter.style.display = 'none';
+        if (document.getElementById('editor').innerText != '') {
+          confirmDiscard();
+        } else {
+          content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
+          filter.style.display = 'none';
+        }
       })
+
+
+
     }
   })
 
   // 与前一种点击页面其他地方隐藏div的实现方法不同，这里利用div后面的遮罩层，给遮罩层绑定事件实现
   filter.addEventListener('click', (e) => {
+    console.log(filter);
     if (document.getElementsByClassName('tweet-frame')[0]) {
-      content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
-      filter.style.display = 'none';
+      if (document.getElementById('editor').innerText != '') {
+        confirmDiscard();
+      } else {
+        content.removeChild(document.getElementsByClassName('tweet-frame')[0]);
+        filter.style.display = 'none';
+      }
     }
   })
 
+
+  function confirmDiscard() {
+    let content = document.getElementsByClassName('content')[0];
+    let confirmDiscard = document.createElement('div');
+    confirmDiscard.classList.add(...['tweet-frame', 'confirm-discard']);
+    confirmDiscard.innerHTML = `
+    <div class="modal-header"><h3 class="modal-title">Discard this Tweet?</h3></div>
+    <div class="modal-body">Are you sure you want to discard this Tweet?</div>
+    <button class="btn close"><i class="fas fa-times"></i></button>
+    <div class="modal-footer">
+    <button class="btn cancel">Cancel</i></button>
+    <button class="btn discard">Discard</button>
+    </div>`
+    content.appendChild(confirmDiscard);
+
+    let filter = document.getElementsByClassName('filter')[0];
+    filter.style.background = 'rgba(0, 0, 0, 0.7)';
+    filter.style['z-index'] = '5002';
+
+    let cancelBtn = document.querySelector('.confirm-discard .cancel');
+    cancelBtn.addEventListener('click', (e) => {
+      content.removeChild(confirmDiscard);
+      filter.style.background = '';
+      filter.style['z-index'] = '';
+    })
+
+    let discBtn = document.querySelector('.confirm-discard .discard');
+    let tweetFrame = document.querySelector('.tweet-frame');
+
+    discBtn.addEventListener('click', (e) => {
+      content.removeChild(confirmDiscard);
+      content.removeChild(tweetFrame);
+      filter.style.display = 'none';
+      filter.style.background = '';
+      filter.style['z-index'] = '';
+    })
+
+    let closeBtn = document.querySelector('.confirm-discard .close');
+    closeBtn.addEventListener('click', (e) => {
+      content.removeChild(confirmDiscard);
+      filter.style.background = '';
+      filter.style['z-index'] = '';
+    })
+  }
 }())
