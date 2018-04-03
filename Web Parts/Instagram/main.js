d80b6ac79d143b0271d8e9d880af38b83ccd3280
Observer-L <observer_l@163.com>
2018-04-03 18:41:10 +0800

    instagram navbar v2.0

--- Web Parts/Instagram/main.js
+++ Web Parts/Instagram/main.js
@@ -1,35 +1,56 @@
 let searchBar = document.getElementById('searchbar');
 let mask = document.getElementById('mask');
 let zoom = document.getElementById('zoom');
+let searchInput = document.querySelector('#searchbar input');
 let placeholder = document.getElementById('placeholder');
-searchBar.addEventListener('click', (e) => {
-  if (!document.querySelector('#searchbar .close-search')) {
-    mask.style.background = '#fff';
-    zoom.style.left = '6px';
-    zoom.style.position = 'absolute'
 
-    mask.style.width = '0';
-    mask.style.border = 'none';
-    mask.style.padding = '0';
-
+// searchBar
+searchBar.addEventListener('click', e => {
+  if (mask.className == '') {
+    mask.classList.add('mask-hide');
+    zoom.classList.add('zoom-hide');
     placeholder.style.display = 'none';
+    document.querySelector('#searchbar input').focus();
+  }
+  e.stopPropagation();
 
-    let closeBtn = document.createElement('span');
-    closeBtn.className = 'close-search';
-    closeBtn.innerHTML = `<i class="fas fa-times-circle"></i>`
-    searchBar.appendChild(closeBtn);
-  } else {
-    mask.style.background = '';
-    zoom.style.left = '';
-    zoom.style.position = ''
-
-    mask.style.width = '';
-    mask.style.border = '';
-    mask.style.padding = '';
-
+  let closeBtn = document.getElementById('close');
+  closeBtn.addEventListener('click', e => {
+    mask.classList.remove('mask-hide');
+    zoom.classList.remove('zoom-hide');
     placeholder.style.display = '';
 
-    searchBar.removeChild(document.querySelector('#searchbar .close-search'));
-  }
+    if (searchInput.value != '') {
+      placeholder.innerText = searchInput.value;
+    } else {
+      placeholder.innerText = '搜索';
+    }
 
+    e.stopPropagation();
+  });
+
+  document.addEventListener('click', e => {
+    mask.classList.remove('mask-hide');
+    zoom.classList.remove('zoom-hide');
+    placeholder.style.display = '';
+    if (searchInput.value != '') {
+      placeholder.innerText = searchInput.value;
+    } else {
+      placeholder.innerText = '搜索';
+    }
+  });
+})
+
+// navbar scroll
+let navbar = document.getElementById('navbar');
+window.addEventListener('scroll', e => {
+  if (window.scrollY > 400) {
+    navbar.style.height = '52px';
+    title.style.opacity = '0';
+    searchBar.style.top = '-15px';
+  } else {
+    navbar.style.height = '';
+    title.style.opacity = '';
+    searchBar.style.top = '';
+  }
 })
